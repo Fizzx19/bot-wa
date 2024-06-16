f5611f8a678e788dc98677d2cd5a1d501048f146
Fizzx19 <157368100+Fizzx19@users.noreply.github.com>
2024-06-16 17:10:49 +0700

    Add files via upload

---
+++ SEARCH/NL-Cpanel/cpanel.js
@@ -0,0 +1,8 @@
+const domain = 'https://rerez.store-panel.biz.id' // Isi Domain Mu
+const apikey = 'ptla_BDklrNVPFO72TOW7sXJSba9d5YzqxIuoTtZdGNw5dAF' // Apikey Plta
+const capikey = 'ptlc_xMdyobdtl2xkJJ5hD79ZYv1OfQvHRVoEiIRcSyARrnj' // Apikey Pltc
+const eggsnya = '15' 
+const location = '1' 
+
+module.exports = { domain, apikey, capikey, eggsnya, location }
+
